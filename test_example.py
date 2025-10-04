f1354cadf397f9d187058b2586f69e8255f77586
Zhuang5-Edison <zhicheng.huang2@exyte.net>
2025-10-05 01:12:40 +0800

    Add files via upload

--- test_example.py
+++ test_example.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import sync_playwright
 
-def handle_ssl_errors():
+def test_example():
     with sync_playwright() as p:
         browser = p.chromium.launch(args=['--ignore-ssl-errors', '--ignore-certificate-errors'])
         context = browser.new_context(ignore_https_errors=True)
