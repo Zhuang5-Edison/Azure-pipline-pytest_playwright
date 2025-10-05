b7964591c3b72600ec061d70ab1586cc64cfc8c3
Zhuang5-Edison <zhicheng.huang2@exyte.net>
2025-10-06 00:13:01 +0800

    Add files via upload

--- test_example_ignore_ssl.py
+++ test_example_ignore_ssl.py
@@ -2,9 +2,9 @@ from playwright.sync_api import sync_playwright
 
 def test_example_ignore_ssl():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+        browser = p.chromium.launch(headless=False)
         context = browser.new_context()
         page = context.new_page()
         page.goto("https://mms.uat.exyte.net/")
-        assert "OneERP UAT" in page.title()
+        print(page.title())
         browser.close()
