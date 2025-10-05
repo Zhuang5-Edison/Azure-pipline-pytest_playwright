49084f42673a8fe638f779c4187017f975b65b4a
Zhuang5-Edison <zhicheng.huang2@exyte.net>
2025-10-05 09:14:57 +0800

    Add files via upload

--- test_example_ignore_ssl.py
+++ test_example_ignore_ssl.py
@@ -1,10 +1,10 @@
-import pytest
+from playwright.sync_api import sync_playwright
 
-@pytest.mark.asyncio
-async def test_example_ignore_ssl(playwright):
-    browser = await playwright.chromium.launch(headless=True)
-    context = await browser.new_context(ignore_https_errors=True)
-    page = await context.new_page()
-    await page.goto("https://www.baidu.com/")
-    assert "Welcome" in await page.title()
-    await browser.close()
+def test_manual_browser():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        context = browser.new_context(ignore_https_errors=True)
+        page = context.new_page()
+        page.goto("https://www.baidu.com/")
+        assert "百度一下" in page.title()
+        browser.close()
